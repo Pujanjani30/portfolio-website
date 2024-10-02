4dfabbe8a6e7d634fe77fbad88ac45afdd880418
Pujan <pujanjani30@gmail.com>
2024-10-02 21:39:45 +0530

    NotFound page added

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import ComingSoon from './components/ComingSoon'
 import Footer from './components/Footer'
+import NotFound from './components/NotFound'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Hero />} />
         <Route path="/about" element={<ComingSoon />} />
         <Route path="/projects" element={<ComingSoon />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
