1b165ae46c52f2d19c6c9c7f02c3512458e61c48
Pujan <pujanjani30@gmail.com>
2024-12-01 13:23:05 +0530

    minor fix

--- portfolio-frontend/src/api/api.js
+++ portfolio-frontend/src/api/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 
-const BASE_URL = 'http://localhost:8000/api/v1';
-// const BASE_URL = 'https://portfolio-pujanjani.onrender.com/api/v1';
+const BASE_URL = import.meta.env.DEV
+  ? 'http://localhost:5000/api/v1'
+  : 'https://portfolio-pujanjani.onrender.com/api/v1';
 
 const api = axios.create({
   baseURL: BASE_URL,
