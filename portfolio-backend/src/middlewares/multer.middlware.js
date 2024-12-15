62d7d860f5900e979a9af56f356f7a5525eaa40d
Pujan <pujanjani30@gmail.com>
2024-12-15 12:00:05 +0530

    path updated

--- portfolio-backend/src/middlewares/multer.middlware.js
+++ portfolio-backend/src/middlewares/multer.middlware.js
@@ -9,14 +9,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Ensure /public/temp directory exists
-const tempDir = path.join(__dirname, "../../public/temp");
+const tempDir = path.join(__dirname, "../../tmp");
 if (!fs.existsSync(tempDir)) {
   fs.mkdirSync(tempDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, "./tmp");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
