ea1061b7f11e8020f8d08f3ff45d6e900131bcfa
Pujan <pujanjani30@gmail.com>
2025-01-08 18:53:50 +0530

    Loading component updated

--- portfolio-frontend/src/components/common/Loading.jsx
+++ portfolio-frontend/src/components/common/Loading.jsx
@@ -4,13 +4,13 @@ const Loading = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen space-y-4">
       <div className="text-center">
-        <span className="text-lg text-gray-800">
+        <span className="text-lg text-white">
           It may take some time to load as the server is hosted on Render Free Tier.
         </span>
       </div>
       <div className="flex items-center space-x-2">
-        <div className="w-5 h-5 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
-        <span className="text-xl text-gray-600">Loading...</span>
+        <div className="w-5 h-5 border-3 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+        <span className="text-xl text-white">Loading...</span>
       </div>
     </div>
   );
